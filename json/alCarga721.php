ca8e5e1b7189f4ab370bc70d0f6d0e4fb88616eb
mariofernandez <siler_hack@hotmail.com>
2014-11-13 18:04:15 -0600

    correcciones

--- json/alCarga721.php
+++ json/alCarga721.php
@@ -3,7 +3,7 @@
 	
 	date_default_timezone_set('America/Mexico_City');
 	include 'alUnidadesLeeArchivo.php';
-	iudsrfyuiewdyfiews
+	iudsrfyuiewdyiews
 	error_reporting(E_ALL);
     $a = array();
 	$a['successTitle'] = "Manejador de Archivos";
